98a5373c47617a3273d9499cefd940545bbd9d07
yangsijie <yangsijie151104@126.com>
2018-11-21 15:38:49 +0800

    修复bug：一旦获得数据，就记录文件的修改时间，防止多次创建

--- RouterConfig/config.py
+++ RouterConfig/config.py
@@ -37,18 +37,32 @@ class ConfigureHandler(object):
                     config_file = self.load_file_handler.load_config_file(first_load=False)
 
             if config_file is not None:
+                self.last_modified_time = self._get_modified_time_by_path(self.config_file_path)
                 self._create_config_process(config_file)
 
             time.sleep(5)
 
     def _listen_file_modified(self):
         """Whether the listener file has been modified."""
-        modified_time = os.stat(self.config_file_path).st_mtime
-        if (self.last_modified_time is not None) and (modified_time == self.last_modified_time):
+        modified_time = self._get_modified_time_by_path(self.config_file_path)
+
+        if (modified_time is None) or (
+                (self.last_modified_time is not None) and (modified_time == self.last_modified_time)):
             return False
         else:
             self.last_modified_time = modified_time
             return True
+
+    @classmethod
+    def _get_modified_time_by_path(cls, file_path):
+        """
+        Get modified time of file by giving its path.
+        :param file_path: file path
+        :return:
+        """
+        if not os.path.exists(file_path):
+            return None
+        return os.stat(file_path).st_mtime
 
     def _create_config_process(self, config_file):
         """Create subprocess to parse and apply the configuration."""
